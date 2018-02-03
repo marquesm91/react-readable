606a5817d1e59b7090dadbe60e9f56677aaa1f0b
Matheus Marques <marquesm91@gmail.com>
2018-02-03 18:47:17 -0200

    refactor: remove store from index.js and create its own folder

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 import App from './App';
-import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
 import './index.css';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(
-      thunk
-    )
-  )
-);
 
 ReactDOM.render(
   <BrowserRouter><App store={store} /></BrowserRouter>,
