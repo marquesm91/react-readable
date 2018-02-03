7c572d34cd41ab9443401f37465133ae5b00221d
Matheus Marques <marquesm91@gmail.com>
2018-02-03 17:49:36 -0200

    feat: Add redux, logger and thunk middleware and set initial config in App.js and index.js. Create a dummy reducer only to test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Hello World!</h1>
+        <h2>{this.props.reducer}</h2>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ reducer }) => {
+  return {
+    reducer: reducer.length === 0 ? 'dummy empty reducer': reducer
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd(action.type);
+  return result;
+}
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(
+      logger,
+      thunk
+    )
+  )
+);
+
+ReactDOM.render(
+  <BrowserRouter><App store={store} /></BrowserRouter>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
