cfb1a104a021f96c37effc1f38f93bad3c11c0a2
Matheus Marques <marquesm91@gmail.com>
2018-02-04 14:02:36 -0200

    feat: Add timestamp conversion to string to date be readable

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { getTimestampAsString } from '../utils';
 
-const Post = ({ post }) => (
-  <div>
-    <h2>{post.body}</h2>
-    <h3>{post.title}</h3>
-    <h3>{post.timestamp}</h3>
-    <h3>{post.author}</h3>
-    <h3>{post.category}</h3>
-    <h3>{post.voteScore}</h3>
-    <h3>{post.deleted}</h3>
-    <h3>{post.commentCount}</h3>
-    <h4>{post.id}</h4>
-  </div>
-);
+const Post = ({ post }) => {
+  const { full } = getTimestampAsString(post.timestamp);
+  return (
+    <div>
+      <h2>{post.body}</h2>
+      <h3>{post.title}</h3>
+      <h3>{full}</h3>
+      <h3>{post.author}</h3>
+      <h3>{post.category}</h3>
+      <h3>{post.voteScore}</h3>
+      <h3>{post.deleted}</h3>
+      <h3>{post.commentCount}</h3>
+      <h4>{post.id}</h4>
+    </div>
+  );
+};
 
 export { Post };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
 const uuidv4 = require('uuid/v4');
 
 export const generateUUID = (options = null) => uuidv4(options);
+
+export const getTimestampAsString = (timestamp = null) => {
+  const date = new Date(timestamp || Date.now());
+
+  const [ dayName, month, day, year, time, timezone ] = date.toString().split(' ');
+
+  return { full: date.toString(), dayName, month, day, year, time, timezone };
+}
