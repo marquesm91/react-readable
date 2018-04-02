2eac0f9860c6c0cc0cf891ebe3e5da4c81d7612d
Matheus Marques <marquesm91@gmail.com>
2018-04-02 18:19:37 -0300

    refactor: Improve getTimestrampString

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ const uuidv4 = require('uuid/v4');
 export const generateUUID = (options = null) => uuidv4(options);
 
 export const getTimestampAsString = (timestamp = null) => {
-  const date = new Date(timestamp || Date.now());
+  const date = timestamp ? new Date(timestamp) : new Date();
 
   const [ dayName, month, day, year, time, timezone ] = date.toString().split(' ');
 
